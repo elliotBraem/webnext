0c276339cfc722b83733641d57418633e7222949
Carina root.akaia.near#0878 Akaia <carina@dmail.ai>
2023-03-30 00:28:08 +0400

    Remove scheduling

--- realms/desktop/src/routes/root.rs
+++ realms/desktop/src/routes/root.rs
@@ -1,10 +1,9 @@
-use {dioxus::prelude::*, rxrust::prelude::*, std::time::Duration};
+use {dioxus::prelude::*, rxrust::prelude::*};
 
 pub fn render(cx: Scope) -> Element {
 	const TEST: &'static str = "Hello Rust community";
 
 	let mut content: String = "".to_string();
-	let threads_scheduler = FuturesThreadPoolScheduler::new().unwrap();
 
 	let numbers = observable::from_iter(0 .. 10);
 	// create an even stream by filter
@@ -13,9 +12,7 @@ pub fn render(cx: Scope) -> Element {
 	let odd = numbers.clone().filter(|v| v % 2 != 0);
 
 	// merge odd and even stream again
-	even.merge(odd)
-	    .delay(Duration::new(0, 3000), threads_scheduler)
-	    .subscribe(|v| content = format!("{v}. {TEST}"));
+	even.merge(odd).subscribe(|v| content = format!("{v}. {TEST}"));
 
 	render!(p { content })
 }
