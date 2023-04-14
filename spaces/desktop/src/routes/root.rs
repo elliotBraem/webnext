b42b5fbe5dad52b3885469fa6f35821521ffb7de
Carina root.akaia.near#0878 Akaia <carina@dmail.ai>
2023-04-14 06:47:40 +0400

    ➖ Stop using rxrust

--- spaces/desktop/src/routes/root.rs
+++ spaces/desktop/src/routes/root.rs
@@ -1,18 +1,9 @@
-use {dioxus::prelude::*, rxrust::prelude::*};
+use {dioxus::prelude::*, rxrust::prelude::*, std::time::Duration};
 
 pub fn render(cx: Scope) -> Element {
-	const TEST: &'static str = "Hello Rust community";
+	const TEST: &str = "Hello Rust community!";
 
-	let mut content: String = "".to_string();
-
-	let numbers = observable::from_iter(0 .. 10);
-	// create an even stream by filter
-	let even = numbers.clone().filter(|v| v % 2 == 0);
-	// create an odd stream by filter
-	let odd = numbers.clone().filter(|v| v % 2 != 0);
-
-	// merge odd and even stream again
-	even.merge(odd).subscribe(|v| content = format!("{v}. {TEST}"));
+	let mut content: String = String::from("");
 
 	render!(p { content })
 }
